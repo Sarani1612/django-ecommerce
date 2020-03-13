66461785533601f6ec14f91061f8d66b9e76f13c
Sarani1612 <sara.larsen1612@gmail.com>
2020-03-13 15:52:53 +0000

    added cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from cart import urls as urls_cart
 from products.views import all_products
 from django.conf import settings
 from django.conf.urls.static import static
@@ -26,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', all_products, name="index"),
     path('accounts/', include(urls_accounts)),
-    path('products/', include(urls_products))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('products/', include(urls_products)),
+    path('cart/', include(urls_cart))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
