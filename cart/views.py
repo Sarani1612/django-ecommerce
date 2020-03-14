670817cdadefdfa17cf87507b6fd2a71262150b9
Sarani1612 <sara.larsen1612@gmail.com>
2020-03-14 17:48:35 +0000

    added search function

--- cart/views.py
+++ cart/views.py
@@ -15,7 +15,7 @@ def add_to_cart(request, id):
 
     cart = request.session.get('cart', {})
     if id in cart:
-        cart[id] = int(cart[id]) + quantity      
+        cart[id] = int(cart[id]) + quantity
     else:
         cart[id] = cart.get(id, quantity) 
 
@@ -23,17 +23,18 @@ def add_to_cart(request, id):
     return redirect(reverse('index'))
 
 
-def adjust_cart(request):
+def adjust_cart(request, id):
     """
-    Adjust the quantity of the specified product to the specified amount
+    Adjust the quantity of the specified product to the specified
+    amount
     """
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
-        cart[id] = quantity
+        cart[str(id)] = quantity
     else:
-        cart.pop(id)
+        cart.pop(str(id))
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
